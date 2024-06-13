7d0548c23c9dea75bf5cf1cc506eb6e83aa13c43
Mnimmakayala <Mnimmakayala1108@conestogac.on.ca>
2024-06-12 22:50:55 -0400

    Add files via upload
    Assignment 2

---
+++ integration_test.py
@@ -0,0 +1,10 @@
+import square_of_sum
+
+#Passing values for sum of two numbers
+def test_sumoftwonos():
+    assert square_of_sum.sumoftwonos(2,2) == 5
+
+#Passing values for multiply of two numbers
+def test_multiplyoftwonos():
+    assert square_of_sum.multiplyoftwonos(2,2) == 8
+
---
+++ square_of_sum.py
@@ -0,0 +1,9 @@
+
+#This method will sum the passed two numbers
+def sumoftwonos(a,b):
+    return (a*a) + (b*b)
+
+#This method will multiply the two numbers
+def multiplyoftwonos(a,b):
+    return 2*a*b
+
